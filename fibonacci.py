452b414180a2132de1494fbd7b1a7d6fcbf1479f
Bishwa-nath <bishwanath.2020@gmail.com>
2024-07-10 21:58:25 +0600

    fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    fib_sequence = []
+    a, b = 0, 1
+    while len(fib_sequence) < n:
+        fib_sequence.append(a)
+        a, b = b, a + b
+    return fib_sequence
+
+# Example usage:
+n = 10  # Number of Fibonacci numbers to generate
+print(fibonacci(n))
